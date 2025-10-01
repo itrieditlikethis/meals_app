631dba7b5d7098796c3cc3669b438ec1561811f1
itrieditlikethis <albert.fedev@gmail.com>
2025-10-01 15:12:55 +0300

    feat: add category list, add navigation to category list

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
+import 'meals_screen.dart';
+
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(title: 'title', mealsData: []),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +29,12 @@ class CategoriesScreen extends StatelessWidget {
           mainAxisSpacing: 20,
         ),
         children: availableCategories
-            .map((item) => CategoryGridItem(category: item))
+            .map(
+              (item) => CategoryGridItem(
+                category: item,
+                onSelectCategory: () => _selectCategory(context),
+              ),
+            )
             .toList(),
       ),
     );
--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 
+import '../widgets/specific_category_list_item.dart';
+
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.mealsData});
 
@@ -10,14 +12,13 @@ class MealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     final Widget content;
     if (mealsData.isNotEmpty) {
       content = ListView.builder(
         itemCount: mealsData.length,
-        itemBuilder: (ctx, index) => Card(
-          key: ValueKey(mealsData[index].title),
-          child: Text(mealsData[index].title),
-        ),
+        itemBuilder: (ctx, index) =>
+            SpecificCategoryListItem(mealsData: mealsData, index: index),
       );
     } else {
       content = Center(child: Text('OOPS'));
@@ -25,6 +26,7 @@ class MealsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text(title)),
       body: content,
+      backgroundColor: theme.scaffoldBackgroundColor,
     );
   }
 }
