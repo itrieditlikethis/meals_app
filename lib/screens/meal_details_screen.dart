588c00a0f491ef298f69c8540238fde8d2029a32
itrieditlikethis <albert.fedev@gmail.com>
2025-10-30 17:04:47 +0200

    feat: add meal details screen, add navigation to this one

---
+++ lib/screens/meal_details_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealDetailsScreen extends StatelessWidget {
+  final Meal meal;
+
+  const MealDetailsScreen({super.key, required this.meal});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(meal.title),
+      ),
+      body: Container(),
+    );
+  }
+
+}
