a0ec3ec60b73070e168855008f877818eb302787
itrieditlikethis <albert.fedev@gmail.com>
2025-11-22 18:20:46 +0200

    feat: add menu tiles to drawer

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -39,8 +39,34 @@ class MainDrawer extends StatelessWidget {
               ],
             ),
           ),
+          _DrawerListTile(tileIcon: Icons.restaurant, tileTitle: 'Meals'),
+          _DrawerListTile(tileIcon: Icons.settings, tileTitle: 'Filters'),
         ],
       ),
+    );
+  }
+}
+
+class _DrawerListTile extends StatelessWidget {
+  const _DrawerListTile({required this.tileIcon, required this.tileTitle});
+
+  final IconData tileIcon;
+  final String tileTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return ListTile(
+      leading: Icon(tileIcon, size: 26, color: theme.colorScheme.primaryFixed),
+      title: Text(
+        tileTitle,
+        style: theme.textTheme.titleSmall!.copyWith(
+          color: theme.colorScheme.primary,
+          fontSize: 24,
+        ),
+      ),
+      onTap: () {},
     );
   }
 }
